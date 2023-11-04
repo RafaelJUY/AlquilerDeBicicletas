a82dcdce9a09ca2afa8d2a9b2bd918eeed5262cc
Rafael E. Cortez <rafael.ecortez@gmail.com>
2023-11-03 23:04:08 -0300

    modificado estacion mas cernada y distancia entre estaciones

--- microservicio-estaciones/src/main/java/com/tpi/bda/microservicioestaciones/service/IEstacionService.java
+++ microservicio-estaciones/src/main/java/com/tpi/bda/microservicioestaciones/service/IEstacionService.java
@@ -1,5 +1,6 @@
 package com.tpi.bda.microservicioestaciones.service;
 
+import com.tpi.bda.microservicioestaciones.model.Ubicacion;
 import com.tpi.bda.microservicioestaciones.model.entity.Estacion;
 
 import java.util.List;
@@ -7,11 +8,13 @@ import java.util.List;
 public interface IEstacionService {
     List<Estacion> findAll();
 
-    Estacion findEstacionCercana(double latitud, double longitud);
-
+//    Estacion findEstacionCercana(double latitud, double longitud);
+    Estacion findEstacionCercana(Ubicacion ubicacion);
     Estacion findEstacionById(long idEstacion);
     
-    double calularDistanciaEstaciones(long idEstacion1, long idEstacion2);
+//    double calularDistanciaEstaciones(long idEstacion1, long idEstacion2);
+    double calularDistancia(Estacion estacion1, Estacion estacion2);
+    double calularDistancia(Ubicacion ubicacion, Estacion estacion);
 
     Estacion crearEstacion(Estacion estacion);
 }
--- microservicio-estaciones/src/main/java/com/tpi/bda/microservicioestaciones/service/impl/EstacionServiceImpl.java
+++ microservicio-estaciones/src/main/java/com/tpi/bda/microservicioestaciones/service/impl/EstacionServiceImpl.java
@@ -1,5 +1,6 @@
 package com.tpi.bda.microservicioestaciones.service.impl;
 
+import com.tpi.bda.microservicioestaciones.model.Ubicacion;
 import com.tpi.bda.microservicioestaciones.model.entity.Estacion;
 import com.tpi.bda.microservicioestaciones.repository.IEstacionRepository;
 import com.tpi.bda.microservicioestaciones.service.IEstacionService;
@@ -21,33 +22,66 @@ public class EstacionServiceImpl implements IEstacionService {
         return this.estacionRepository.findAll();
     }
 
-    @Override
-    public Estacion findEstacionCercana(double latitud, double longitud) {
-        List<Estacion> estaciones = this.findAll();
-        Estacion estacionCercana = estaciones.get(0);
-        double menorDistancia = 110000 * Math.sqrt(Math.pow(estacionCercana.getLatitud() - latitud, 2) +
-                Math.pow(estacionCercana.getLongitud() - longitud,2));
+//    @Override
+//    public Estacion findEstacionCercana(double latitud, double longitud) {
+//        List<Estacion> estaciones = this.findAll();
+//        Estacion estacionCercana = estaciones.get(0);
+//        double menorDistancia = 110000 * Math.sqrt(Math.pow(estacionCercana.getLatitud() - latitud, 2) +
+//                Math.pow(estacionCercana.getLongitud() - longitud,2));
+//
+//        for (Estacion estacion : estaciones) {
+//            double distancia = 110000 * Math.sqrt(Math.pow(estacion.getLatitud() - latitud, 2) +
+//                    Math.pow(estacion.getLongitud() - longitud,2));
+//
+//            if (distancia < menorDistancia) {
+//                estacionCercana = estacion;
+//                menorDistancia = distancia;
+//            }
+//        }
+//
+//        return estacionCercana;
+//    }
+        @Override
+        public Estacion findEstacionCercana(Ubicacion ubicacion) {
+            List<Estacion> estaciones = this.findAll();
 
-        for (Estacion estacion : estaciones) {
-            double distancia = 110000 * Math.sqrt(Math.pow(estacion.getLatitud() - latitud, 2) +
-                    Math.pow(estacion.getLongitud() - longitud,2));
+            Estacion estacionCercana = estaciones.get(0);
+            double menorDistancia = calularDistancia(ubicacion, estacionCercana);
 
-            if (distancia < menorDistancia) {
-                estacionCercana = estacion;
-                menorDistancia = distancia;
+            double distancia;
+            for (Estacion estacion : estaciones) {
+                distancia = calularDistancia(ubicacion, estacion);
+                if (menorDistancia > distancia) {
+                    menorDistancia = distancia;
+                    estacionCercana = estacion;
+                }
             }
+            return estacionCercana;
         }
 
-        return estacionCercana;
-    }
+//    @Override
+//    public double calularDistanciaEstaciones(long idEstacion1, long idEstacion2){
+//        Estacion estacion1 = findEstacionById(idEstacion1);
+//        Estacion estacion2 = findEstacionById(idEstacion2);
+//
+//        double distancia = 110000 * Math.sqrt(Math.pow(estacion1.getLatitud() - estacion2.getLatitud(), 2) +
+//                Math.pow(estacion1.getLongitud() - estacion2.getLongitud(),2));
+//
+//        return distancia;
+//    }
     @Override
-    public double calularDistanciaEstaciones(long idEstacion1, long idEstacion2){
-        Estacion estacion1 = findEstacionById(idEstacion1);
-        Estacion estacion2 = findEstacionById(idEstacion2);
-
+    public double calularDistancia(Estacion estacion1, Estacion estacion2){
         double distancia = 110000 * Math.sqrt(Math.pow(estacion1.getLatitud() - estacion2.getLatitud(), 2) +
                 Math.pow(estacion1.getLongitud() - estacion2.getLongitud(),2));
-        
+
+        return distancia;
+    }
+
+    @Override
+    public double calularDistancia(Ubicacion ubicacion, Estacion estacion){
+        double distancia = 110000 * Math.sqrt(Math.pow(ubicacion.getLatitud() - estacion.getLatitud(), 2) +
+                Math.pow(ubicacion.getLongitud() - estacion.getLongitud(),2));
+
         return distancia;
     }
     @Override
