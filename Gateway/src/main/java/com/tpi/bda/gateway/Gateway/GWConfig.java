5622ccfa17548e32b9e83fa2053f6b5aeecf9382
Tomás Tiseira <85915@sistemas.frc.utn.edu.ar>
2023-11-10 20:26:32 -0300

    Autenticación de clientes y restricción de funcionamiento por roles

--- Gateway/src/main/java/com/tpi/bda/gateway/Gateway/GWConfig.java
+++ Gateway/src/main/java/com/tpi/bda/gateway/Gateway/GWConfig.java
@@ -5,6 +5,13 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtGrantedAuthoritiesConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class GWConfig {
@@ -18,6 +25,42 @@ public class GWConfig {
                 // Ruteo al Microservicio de alquileres
                 .route(p -> p.path("/api/alquileres/**").uri(uriAlquileres))
                 .build();
+    }
 
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
+        http.authorizeExchange(exchanges -> exchanges
+
+                        // Rutas para ADMINISTRADOR
+                        .pathMatchers(HttpMethod.POST, "/api/estaciones").hasRole("ADMINISTRADOR")
+                        .pathMatchers("/api/alquileres").hasRole("ADMINISTRADOR")
+
+                        // Rutas para CLIENTE
+                        .pathMatchers("/api/estaciones/**").hasRole("CLIENTE")
+                        .pathMatchers(HttpMethod.POST, "/api/alquileres/").hasRole("CLIENTE")
+                        .pathMatchers(HttpMethod.PATCH, "/api/alquileres/").hasRole("CLIENTE")
+                        // Otras rutas permitidas para todos los roles autenticados
+                        .anyExchange().authenticated()
+
+                ).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
+        var jwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
+        var grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+        // Se especifica el nombre del claim a analizar
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
+        // Se agrega este prefijo en la conversión por una convención de Spring
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+
+        // Se asocia el conversor de Authorities al Bean que convierte el token JWT a un objeto Authorization
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(
+                new ReactiveJwtGrantedAuthoritiesConverterAdapter(grantedAuthoritiesConverter));
+
+        return jwtAuthenticationConverter;
     }
 }
