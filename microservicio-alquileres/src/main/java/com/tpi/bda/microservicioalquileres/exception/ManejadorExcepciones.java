a966a62c22de51a1b3daa23fb48a1b2b5a39d3ea
Rafael E. Cortez <rafael.ecortez@gmail.com>
2023-11-12 03:50:22 -0300

    paquete exception: agregado manejador de excepciones

---
+++ microservicio-alquileres/src/main/java/com/tpi/bda/microservicioalquileres/exception/ManejadorExcepciones.java
@@ -0,0 +1,26 @@
+package com.tpi.bda.microservicioalquileres.exception;
+
+import com.tpi.bda.microservicioalquileres.exception.personalized.EntidadNoExistenteException;
+import com.tpi.bda.microservicioalquileres.exception.personalized.SinRegistrosDisponiblesExeption;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ManejadorExcepciones {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> manejadorExeption(Exception ex){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(EntidadNoExistenteException.class)
+    public ResponseEntity<String> manejadorEntidadNoExistenteException(EntidadNoExistenteException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(SinRegistrosDisponiblesExeption.class)
+    public ResponseEntity<String> manejadorSinRegistrosDisponiblesExeption(SinRegistrosDisponiblesExeption ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
